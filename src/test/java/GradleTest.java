fa6ddda968be3a196ddf2756dee7f9851e257eae
Bogdan Tyshkovets <btishkovets@gmail.com>
2021-09-08 22:37:15 +0300

    sec

--- src/test/java/GradleTest.java
+++ src/test/java/GradleTest.java
@@ -13,4 +13,18 @@ public class GradleTest {
         open("https://www.google.com/");
         closeWebDriver();
     }
+
+    @Test
+    public void  test2(){
+
+        open("https://www.google.com/");
+        closeWebDriver();
+    }
+
+    @Test
+    public void  test3(){
+
+        open("https://www.google.com/");
+        closeWebDriver();
+    }
 }
